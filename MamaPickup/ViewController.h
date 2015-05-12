389d28993fe7156c77d288f637447987b10b49e1
Dai Yue <yue.dai@qunar.com>
2015-05-12 15:07:44 +0800

    Initial commit.

---
+++ MamaPickup/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MamaPickup
+//
+//  Created by yue.dai on 15/5/10.
+//  Copyright (c) 2015年 Meaogoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MamaPickup/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MamaPickup
+//
+//  Created by yue.dai on 15/5/10.
+//  Copyright (c) 2015年 Meaogoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
